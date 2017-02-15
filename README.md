d26c04fbe972e6eae474bfb04db8c217be709a7f
Kaya <kayalion@kayalion.net>
2017-02-15 17:58:44 +0100

    initial release

---
+++ README.md
@@ -0,0 +1,17 @@
+# Ride: Mail (Queue)
+
+Use a queue to send mails from your Ride application.
+
+## Related Modules 
+
+- [ride/app](https://github.com/all-ride/ride-app)
+- [ride/lib-mail](https://github.com/all-ride/ride-lib-mail)
+- [ride/lib-queue](https://github.com/all-ride/ride-lib-queue)
+
+## Installation
+
+You can use [Composer](http://getcomposer.org) to install this application.
+
+```
+composer require ride/app-mail-queue
+```
---
+++ src/ride/application/mail/transport/QueueMailTransport.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace ride\application\mail\transport;
+
+use ride\application\queue\job\MailQueueJob;
+
+use ride\library\mail\transport\Transport;
+use ride\library\mail\MailMessage;
+use ride\library\queue\dispatcher\QueueDispatcher;
+
+/**
+ * Interface for the mail transport
+ */
+class QueueMailTransport implements Transport {
+
+    /**
+     * Instance of the queue dispatcher
+     * @var \ride\library\queue\dispatcher\QueueDispatcher
+     */
+    private $queueDispatcher;
+
+    /**
+     * Instance of the mail transport
+     * @var \ride\library\mail\transport\Transport
+     */
+    private $mailTransport;
+
+    /**
+     * Constructs a new queue mail transport
+     * @param \ride\library\queue\dispatcher\QueueDispatcher $queueDispatcher
+     * @param \ride\library\mail\transport\Transport $mailTransport
+     * @return null
+     */
+    public function __construct(QueueDispatcher $queueDispatcher, Transport $mailTransport) {
+        $this->queueDispatcher = $queueDispatcher;
+        $this->mailTransport = $mailTransport;
+    }
+
+    /**
+     * Creates a mail message
+     * @return \ride\library\mail\MailMessage
+     */
+    public function createMessage() {
+        return $this->mailTransport->createMessage();
+    }
+
+    /**
+     * Delivers a mail message
+     * @param \ride\library\mail\MailMessage $message
+     * @return null
+     */
+    public function send(MailMessage $message) {
+        $queueJob = new MailQueueJob();
+        $queueJob->setMailTransport($this->mailTransport);
+        $queueJob->setMailMessage($message);
+
+        $this->queueDispatcher->queue($queueJob);
+    }
+
+}
